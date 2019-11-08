37322059029fe71bf7dbd0c74aace4ef849c69a1
Jamie Goodnight <goodnightlambda@gmail.com>
2019-11-08 13:07:31 -0500

    :bug: Realized count wasn't passing test because of global variable. Fixed

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,18 +3,14 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
-count = 0
 
 
 def count_th(word):
-    global count
     if len(word) < 2:
-        return count
+        return 0
     if word[0] + word[1] == "th":
-        count = count + 1
         newWord = word[1:]
-        count_th(newWord)
+        return 1 + count_th(newWord)
     if word[0] + word[1] != "th":
         newWord = word[1:]
-        count_th(newWord)
-    return count
+        return 0 + count_th(newWord)
